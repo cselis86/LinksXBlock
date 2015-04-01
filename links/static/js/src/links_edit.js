15f7f278f4ae4c738151e6096881d02b663b34d8
Elis Hazisllari <cs_elis@live.com>
2015-04-01 03:13:24 +0300

    test

--- links/static/js/src/links_edit.js
+++ links/static/js/src/links_edit.js
@@ -7,22 +7,11 @@ function LinksEditXBlock(runtime, element) {
       href3: $(element).find('input[name=href3]').val(),
       href4: $(element).find('input[name=href3]').val(),  
     };
-    if (data['href1']==''||data['href1'] == null){
-	   alert('type something at Link 1');
+
+    if (data.href1 === null ) {
+        $.post(handlerUrl, JSON.stringify(data)).done(function(response) { });
     }
-    else if (data['href2'] == ''|| data['href2'] == null){
-        alert('type something at Link 2');
-    }
-    else if (data['href3'] == ''|| data['href3'] == null){
-        alert('type something at Link 3');
-    }
-    else if (data['href4'] == '' || data['href4'] == null){
-        alert('type something at Link 4');
-    }    
-    else{
-        $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
-    }
-   });
+  
   });
 
   $(element).find('.cancel-button').bind('click', function() {
